5c32c340207e56be10bcb71f7fe3c5bfbf11a115
Justin Lennox <justinanthonylennox@gmail.com>
2016-03-15 00:59:24 -0400

    Checking Empty and Highlighting Working

--- fifteen.js
+++ fifteen.js
@@ -22,6 +22,7 @@ var square11;
 var square12;
 var square13;
 var square14;
+var square15;
 var square16;
 
 var puzzleArray;
@@ -76,12 +77,32 @@ function shuffle() {
 	}
 }
 
+
 function MouseOverSquare(squareNumber){
-    CheckNeighborEmpty(squareNumber);
+    if(CheckNeighborEmpty(squareNumber)){
+        highlight(squareNumber);
+    }
 }
 
+function MouseOffSquare(squareNumber){
+    unhighlight(squareNumber);
+}
+
+function highlight(squareNumber){
+    puzzleArray[squareNumber - 1].style.borderColor = "red";
+}
+
+function unhighlight(squareNumber){
+    puzzleArray[squareNumber - 1].style.borderColor = "black";
+}
+
+
 function CheckNeighborEmpty(squareNumber){
-    alert(squareNumber);
+    if((!puzzleArray[squareNumber] && squareNumber < 16) || (!puzzleArray[squareNumber - 2] && squareNumber > 1) || (!puzzleArray[squareNumber - 5] && squareNumber > 4) || (!puzzleArray[squareNumber + 3] && squareNumber < 13)){ //Checks the square to the right
+        return true;
+    }else{
+        return false;
+    }
 }
 
 function setSquareVariables(){
@@ -106,46 +127,90 @@ function addSquareListeners(){
     square1.addEventListener("mouseover", function(){
                              MouseOverSquare(1);
                              });
+    square1.addEventListener("mouseout", function(){
+                             MouseOffSquare(1);
+                             });
     square2.addEventListener("mouseover", function(){
                              MouseOverSquare(2);
+                             });
+    square2.addEventListener("mouseout", function(){
+                             MouseOffSquare(2);
                              });
     square3.addEventListener("mouseover", function(){
                              MouseOverSquare(3);
                              });
+    square3.addEventListener("mouseout", function(){
+                             MouseOffSquare(3);
+                             });
     square4.addEventListener("mouseover", function(){
                              MouseOverSquare(4);
+                             });
+    square4.addEventListener("mouseout", function(){
+                             MouseOffSquare(4);
                              });
     square5.addEventListener("mouseover", function(){
                              MouseOverSquare(5);
                              });
+    square5.addEventListener("mouseout", function(){
+                             MouseOffSquare(5);
+                             });
     square6.addEventListener("mouseover", function(){
                              MouseOverSquare(6);
+                             });
+    square6.addEventListener("mouseout", function(){
+                             MouseOffSquare(6);
                              });
     square7.addEventListener("mouseover", function(){
                              MouseOverSquare(7);
                              });
+    square7.addEventListener("mouseout", function(){
+                             MouseOffSquare(7);
+                             });
     square8.addEventListener("mouseover", function(){
                              MouseOverSquare(8);
                              });
+    square8.addEventListener("mouseout", function(){
+                             MouseOffSquare(8);
+                             });
     square9.addEventListener("mouseover", function(){
                              MouseOverSquare(9);
-                             });
+                             });square9.addEventListener("mouseout", function(){
+                                                         MouseOffSquare(9);
+                                                         });
     square10.addEventListener("mouseover", function(){
-                             MouseOverSquare(10);
-                             });
+                              MouseOverSquare(10);
+                              });
+    square10.addEventListener("mouseout", function(){
+                              MouseOffSquare(10);
+                              });
     square11.addEventListener("mouseover", function(){
-                             MouseOverSquare(11);
-                             });
+                              MouseOverSquare(11);
+                              });
+    square11.addEventListener("mouseout", function(){
+                              MouseOffSquare(11);
+                              });
     square12.addEventListener("mouseover", function(){
-                             MouseOverSquare(12);
-                             });
+                              MouseOverSquare(12);
+                              });
+    square12.addEventListener("mouseout", function(){
+                              MouseOffSquare(12);
+                              });
     square13.addEventListener("mouseover", function(){
-                             MouseOverSquare(13);
-                             });
+                              MouseOverSquare(13);
+                              });
+    square13.addEventListener("mouseout", function(){
+                              MouseOffSquare(13);
+                              });
     square14.addEventListener("mouseover", function(){
-                             MouseOverSquare(14);
-                             });
+                              MouseOverSquare(14);
+                              });
+    square14.addEventListener("mouseout", function(){
+                              MouseOffSquare(14);
+                              });
     square15.addEventListener("mouseover", function(){
-                             MouseOverSquare(15);
-                             });
+                              MouseOverSquare(15);
+                              });
+    square15.addEventListener("mouseout", function(){
+                              MouseOffSquare(15);
+                              });
 }
